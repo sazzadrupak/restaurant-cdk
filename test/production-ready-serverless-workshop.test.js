287dd0fbece0b9e0d3a8b84dc5fcf1bc504d6b5d
Sazzad Rupak <rupak.sazzad@sniffie.fi>
2025-06-28 20:56:11 +0300

    Initial commit

---
+++ test/production-ready-serverless-workshop.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ProductionReadyServerlessWorkshop = require('../lib/production-ready-serverless-workshop-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/production-ready-serverless-workshop-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ProductionReadyServerlessWorkshop.ProductionReadyServerlessWorkshopStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
